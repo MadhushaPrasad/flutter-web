1840b28459c03ed4d6a36caade001070f5daf684
MadhushaPrasad <madushaprasad21@gmail.com>
2023-06-19 23:21:19 +0530

    Text color is hard to read issue

--- lib/main.dart
+++ lib/main.dart
@@ -70,11 +70,15 @@ class BigCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(pair.asLowerCase),
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
